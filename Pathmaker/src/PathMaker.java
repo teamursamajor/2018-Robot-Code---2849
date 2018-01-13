0b05e2f2db4eb88e65bb22ab097bf65f868543c5
TheSphereWizard <brian.horse@gmail.com>
2018-01-13 14:58:47 -0500

    Updated PathMaker at Charlie's request

--- Pathmaker/src/PathMaker.java
+++ Pathmaker/src/PathMaker.java
@@ -58,7 +58,8 @@ public class PathMaker {
 		for(int i =0;i<path.size()-1;i++) {
 			output.add(new PointonPath(Math.sqrt(Math.pow(path.get(i).xft-path.get(i+1).xft, 2)+Math.pow(path.get(i).yft-path.get(i+1).yft, 2)),negmod(Math.atan2(path.get(i).yft-path.get(i+1).yft, path.get(i).xft-path.get(i+1).xft),Math.PI*2)*(180/Math.PI),path.get(i).xft,path.get(i).xft));
 		}
-		PathWriter.write(new Path("output",output));
+//		PathWriter.write(new Path("output",output));
+		System.out.println("outputed");
 	}
 	private static double negmod(double atan2,double modvalue) {
 		while(atan2<0) {
@@ -196,6 +197,27 @@ public class PathMaker {
 		pointpanel.setSize(325, 800);
 		frame.add(pointpanel);
 		frame.add(Scrollpanel);
+		JPanel menupanel = new JPanel() {
+			public void paint(Graphics g) {
+				g.fillRect(0, 0, 325, 50);
+				g.setColor(Color.white);
+				g.fill3DRect(10, 10, 30, 30, true);
+			}
+		};
+		menupanel.addMouseListener(new MouseListener() {
+			public void mouseClicked(MouseEvent e) {
+				if(e.getX()>10&e.getX()<40&e.getY()>10&e.getY()<40) {
+					output();
+				}
+			}
+			public void mouseEntered(MouseEvent e) {}
+			public void mouseExited(MouseEvent e) {}
+			public void mousePressed(MouseEvent e) {}
+			public void mouseReleased(MouseEvent e) {}
+		});
+		menupanel.setSize(325,50);
+		menupanel.setLocation(625,0);
+		frame.add(menupanel);
 		frame.setVisible(true);
 	}
 	static int slow=0;
