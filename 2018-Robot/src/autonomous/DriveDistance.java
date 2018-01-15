416c9ce534e28c2a290588f9c3ce21625637faf5
teamursamajor <teamursamajor@gmail.com>
2018-01-15 15:31:28 -0500

    Made DriveDistance work and made the UrsaScript_AutoBuilder file, aka the interpreter for the auto script files

--- 2018-Robot/src/autonomous/DriveDistance.java
+++ 2018-Robot/src/autonomous/DriveDistance.java
@@ -8,16 +8,17 @@ private int distance;
 		this.distance = distance;
 	}
 
+	/** overshoots 3 in */
 	public void runTask() {
 		double leftPowerConstant = 0.3;
 		double rightPowerConstant = 0.3;
 		Drive.resetEncoders();
-		while ((Drive.getLeftEncoder() < distance) && (Drive.getRightEncoder() < distance)) {
+		while (Math.abs(Drive.getLeftEncoder()) < Math.abs(distance) && Math.abs(Drive.getRightEncoder()) < Math.abs(distance)) {
 			//TODO fix condition for negative distances
-			if (Drive.getLeftEncoder() > distance) {
+			if (Math.abs(Drive.getLeftEncoder()) > Math.abs(distance)) {
 				leftPowerConstant = 0;
 			}
-			if (Drive.getRightEncoder() > distance) {
+			if (Math.abs(Drive.getRightEncoder()) > Math.abs(distance)) {
 				rightPowerConstant = 0;
 			}
 			Drive.drive(leftPowerConstant*Math.signum(distance), rightPowerConstant*Math.signum(distance), false);
