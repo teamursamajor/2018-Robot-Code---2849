2e6e64b50eccd1b57d9e143ea90b24dc20dc6f0c
Acer <teamursamajor@gmail.com>
2018-01-29 18:05:06 -0500

    Preliminary Vision code

---
+++ 2018-Robot/src/org/usfirst/frc/team2849/robot/Vision.java
@@ -0,0 +1,31 @@
+package org.usfirst.frc.team2849.robot;
+
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.CvSource;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class Vision extends IterativeRobot {
+    public void robotInit() {
+        new Thread(() -> {
+            UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+            camera.setResolution(640, 480);
+            
+            CvSink cvSink = CameraServer.getInstance().getVideo();
+            CvSource outputStream = CameraServer.getInstance().putVideo("Blur", 640, 480);
+            
+            Mat source = new Mat();
+            Mat output = new Mat();
+            
+            while(!Thread.interrupted()) {
+                cvSink.grabFrame(source);
+                Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
+                outputStream.putFrame(output);
+            }
+        }).start();
+    }
+}
