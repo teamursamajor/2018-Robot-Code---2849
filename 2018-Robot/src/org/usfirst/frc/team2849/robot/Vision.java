10fc270dc28890d1546660f78e52b6b68d36bf31
teamursamajor <teamursamajor@gmail.com>
2018-01-30 16:31:45 -0500

    Made vision class

---
+++ 2018-Robot/src/org/usfirst/frc/team2849/robot/Vision.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team2849.robot;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.CvSource;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class Vision {
+
+}
