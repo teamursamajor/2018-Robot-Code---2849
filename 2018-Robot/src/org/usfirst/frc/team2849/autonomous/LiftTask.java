bd399197d0656e210f8c505619911f68c3248c91
kingeinstein <35170920+kingeinstein@users.noreply.github.com>
2018-01-29 16:53:35 -0800

    Fixed merge on LiftTask

--- 2018-Robot/src/org/usfirst/frc/team2849/autonomous/LiftTask.java
+++ 2018-Robot/src/org/usfirst/frc/team2849/autonomous/LiftTask.java
@@ -1,54 +1,3 @@
-<<<<<<< HEAD
-package org.usfirst.frc.team2849.autonomous;
-
-import org.usfirst.frc.team2849.controls.AutoControl;
-import org.usfirst.frc.team2849.controls.ControlLayout;
-
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.Encoder;
-
-public class LiftTask extends AutoTask {
-
-	private double liftHeight = 0;
-	private Encoder enMotor;
-	private DigitalInput limitSwitch = new DigitalInput(1);
-	
-	//TODO change this value
-	double inchesPerTick = 0.011505d;
-
-	public LiftTask(ControlLayout cont, double height) {
-		super(cont);
-		liftHeight = height;
-		enMotor = new Encoder(5, 4);
-		enMotor.setDistancePerPulse(inchesPerTick);
-	}
-
-	@Override
-	public void run() {
-		int tempHeight = 0;
-		double error = 0.1;
-		while (tempHeight + error > calcHeight() && tempHeight - error < calcHeight()) {
-			double powerConstant = 0.3d;
-			cont.setLiftPower(powerConstant);
-			if (limitSwitch.get()) {
-				enMotor.reset();
-			}
-		}
-	}
-
-	public double calcHeight() {
-		// TODO better than this please
-		double distance = enMotor.getDistance();
-		// TODO 2 carriage code
-
-		return distance;
-	}
-
-	public String toString() {
-		return "LiftTask: " + liftHeight + "\n";
-	}
-}
-=======
 package org.usfirst.frc.team2849.autonomous;
 
 import org.usfirst.frc.team2849.controls.ControlLayout;
@@ -97,4 +46,3 @@ public class LiftTask extends AutoTask {
 		return "LiftTask: " + liftHeight + "\n";
 	}
 }
->>>>>>> branch 'master' of https://github.com/teamursamajor/2018-Robot-Code---2849
