88459b3a7e349048662de2361ad36bcb2c258de9
Ayonator77 <ayodejiodetola@gmail.com>
2018-02-01 16:32:33 -0500

    Merging, still need to resolve some errors tho

--- 2018-Robot/src/org/usfirst/frc/team2849/controls/ControlLayout.java
+++ 2018-Robot/src/org/usfirst/frc/team2849/controls/ControlLayout.java
@@ -1,44 +1,3 @@
-<<<<<<< HEAD
-
-package org.usfirst.frc.team2849.controls;
-
-import org.usfirst.frc.team2849.autonomous.IntakeTask.IntakeType;
-import org.usfirst.frc.team2849.robot.Drive.DriveControl;
-import org.usfirst.frc.team2849.robot.Intake.IntakeControl;
-
-import edu.wpi.first.wpilibj.Spark;
-
-public interface ControlLayout {
-	
-	public double getLeftPower();
-	public double getRightPower();
-	
-	public void setPower(double leftPower, double rightPower);
-	
-	public DriveControl getDrive(Spark frontLeft, Spark frontRight, Spark rearLeft, Spark rearRight);
-	
-	public IntakeControl getIntake(Spark left, Spark right);
-	
-	public void runIntake();
-	
-    public void setIntakeValue( double intakeValue);
-    
-    public double getIntakeValue();
-    
-    public void setLiftPower(double liftPower);
-    
-    public double getLiftPower();
-    
-    public void setLiftHeight(double liftHeight);
-    
-    /**
-     * 
-     * @return lift height in inches
-     */
-    public double getLiftHeight();
-    
-    public void runLift();
-=======
 package org.usfirst.frc.team2849.controls;
 
 import org.usfirst.frc.team2849.autonomous.IntakeTask.IntakeType;
@@ -73,5 +32,4 @@ public interface ControlLayout {
     public void setLiftHeight(double liftHeight);
  
     public double getLiftHeight();
->>>>>>> branch 'master' of https://github.com/teamursamajor/2018-Robot-Code---2849
 }
