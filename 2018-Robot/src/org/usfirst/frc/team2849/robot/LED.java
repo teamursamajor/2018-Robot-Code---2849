6a69ca0780259f2756baffa3c46470b79d5a8152
Acer <teamursamajor@gmail.com>
2018-02-03 16:33:16 -0500

    Add LEDs

--- 2018-Robot/src/org/usfirst/frc/team2849/robot/LED.java
+++ 2018-Robot/src/org/usfirst/frc/team2849/robot/LED.java
@@ -1,5 +1,30 @@
 package org.usfirst.frc.team2849.robot;
 
-public class LED {
+import org.usfirst.frc.team2849.controls.ControlLayout;
 
+import edu.wpi.first.wpilibj.Solenoid;
+
+public class LED extends Thread {
+
+	private ControlLayout cont;
+	private Solenoid  rLED;
+	private Solenoid  gLED;
+	private Solenoid  bLED;
+	
+	public LED(ControlLayout cont) {
+		this.cont = cont;
+		rLED = new Solenoid(0);
+		gLED = new Solenoid(1);
+		bLED = new Solenoid(2);
+		this.start();
+	}
+	
+	public void run() {
+		while (true) {
+			rLED.set(cont.getR());
+			gLED.set(cont.getG());
+			bLED.set(cont.getB());
+		}
+	}
+	
 }
