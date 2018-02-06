1def5cf5ef4615b673f4e9f961ab7c7bdedbdd4e
Punchline <charlies1999@gmail.com>
2018-02-06 17:57:52 -0500

    Converted Path and PathMaker to work with inches.

--- 2018-Robot/src/org/usfirst/frc/team2849/path/TrapVelocityProfile.java
+++ 2018-Robot/src/org/usfirst/frc/team2849/path/TrapVelocityProfile.java
@@ -1,22 +1,25 @@
 package org.usfirst.frc.team2849.path;
+
 import java.util.ArrayList;
 
+import org.usfirst.frc.team2849.util.AngleHelper;
+
 public class TrapVelocityProfile {
-	
+
 	public class Node {
-		
+
 		private double time;
 		private double dist;
 		private double vel;
 		private double acc;
-		
-		public Node (double time, double dist, double vel, double acc) {
+
+		public Node(double time, double dist, double vel, double acc) {
 			this.time = time;
 			this.dist = dist;
 			this.vel = vel;
 			this.acc = acc;
 		}
-		
+
 		public double getDist() {
 			return dist;
 		}
@@ -32,13 +35,13 @@ public class TrapVelocityProfile {
 		public double getAcc() {
 			return acc;
 		}
-		
+
 		public String toString() {
 			return time + ": " + dist + ", " + vel + ", " + acc;
 		}
-		
+
 	}
-	
+
 	private double maxAccel;
 	private double maxVel;
 	private double dt;
@@ -49,18 +52,45 @@ public class TrapVelocityProfile {
 	private double totalTime;
 	private double accelDist;
 	private double cruiseDist;
-	
-	private ArrayList<Node> profile;
-	
-	public TrapVelocityProfile(double maxAccel, double maxVel, double dt, double distance) {
+	private Path path;
+	private double totalDist;
+
+	private Path mappedPath;
+
+	/**
+	 * Trapezoidal velocity profile for mapping velocity, acceleration, and time to
+	 * a Path.
+	 * 
+	 * @param maxAccel
+	 *            Max acceleration in in/s^2
+	 * @param maxVel
+	 *            Max velocity in in/s
+	 * @param dt
+	 *            Time between each point in s
+	 * @param distance
+	 *            Total distance of the Path in inches
+	 */
+	public TrapVelocityProfile(double maxAccel, double maxVel, double dt, Path path) {
 		this.maxAccel = maxAccel;
 		this.maxVel = maxVel;
 		this.dt = dt;
-		this.distance = distance;
-		profile = new ArrayList<Node>();
+		this.path = path;
+		this.totalDist = 0;
+		this.distance = path.get(path.numPoints() - 1).getPosition();
+		mappedPath = new Path();
 		makeProfile();
 	}
-	
+
+	/*
+	 * private void makeProfile() { accelTime = maxVel / maxAccel; accelDist =
+	 * (maxAccel / 2) * Math.pow(accelTime, 2); if (accelDist > distance / 2) {
+	 * accelDist -= (accelDist * 2 - distance) / 2; accelTime = Math.sqrt(2 *
+	 * accelDist / maxAccel); } cruiseDist = distance - (2 * accelDist); cruiseTime
+	 * = cruiseDist / maxVel; totalTime = 2 * accelTime + cruiseTime; for (double t
+	 * = 0; t <= totalTime; t += dt) { profile.add(makePoint(t)); }
+	 * profile.add(makePoint(totalTime)); }
+	 */
+
 	private void makeProfile() {
 		accelTime = maxVel / maxAccel;
 		accelDist = (maxAccel / 2) * Math.pow(accelTime, 2);
@@ -71,43 +101,138 @@ public class TrapVelocityProfile {
 		cruiseDist = distance - (2 * accelDist);
 		cruiseTime = cruiseDist / maxVel;
 		totalTime = 2 * accelTime + cruiseTime;
-		for (double t = 0; t <= totalTime; t += dt) {
-			profile.add(makeNode(t));
+		maxVel = maxAccel * accelTime;
+
+		System.out.println("Acceleration time: " + accelTime);
+		System.out.println("Acceleration distance: " + accelDist);
+		System.out.println("Cruise time: " + cruiseTime);
+		System.out.println("Cruise Distance: " + cruiseDist);
+
+		System.out.println("Total calculated time: " + totalTime);
+		System.out.println("Total given distance: " + distance);
+		double ds;
+		double dir;
+		double xft;
+		double yft;
+		PointonPath toAdd;
+		PointonPath[] points;
+		mappedPath
+				.add(new PointonPath(0, path.get(0).getDirection(), path.get(0).xft, path.get(0).yft, 0, 0, maxAccel));
+		int lastIndex = 0;
+		for (double t = dt; t < totalTime; t += dt) {
+			if (t < accelTime) {
+				System.out.println("t < accelTime");
+				System.out.println("curVel: " + curVel);
+				System.out.println("totalDist: " + totalDist);
+				System.out.println("");
+				curVel = maxAccel * t;
+				totalDist = (maxAccel / 2) * Math.pow(t, 2);
+				ds = totalDist - mappedPath.get(lastIndex).getPosition();
+				points = path.findSurroundingPoints(totalDist);
+				System.out.println("Direction 0: " + points[0].getDirection());
+				System.out.println("Direction 1: " + points[1].getDirection());
+				System.out.println("Smallest Angle: " + AngleHelper.getSmallestAngleBetween(points[0].getDirection(), points[1].getDirection()));
+				System.out.println("Distance difference: " + (totalDist - points[0].getPosition()));
+				System.out.println("");
+				dir = points[0].getDirection() + (totalDist - points[0].getPosition())
+						* (AngleHelper.getSmallestAngleBetween(points[0].getDirection(), points[1].getDirection())
+								/ (points[1].getPosition() - points[0].getPosition()));
+				xft = points[0].xft + Math.cos(Math.toRadians(dir)) * (ds / 12);
+				yft = points[0].yft + Math.sin(Math.toDegrees(dir)) * (ds / 12);
+				toAdd = new PointonPath(totalDist, dir, xft, yft, t, curVel, maxAccel);
+			} else if (t < accelTime + cruiseTime) {
+				totalDist = accelDist + maxVel * (t - accelTime);
+				ds = totalDist - mappedPath.get(lastIndex).getPosition();
+				points = path.findSurroundingPoints(totalDist);
+				dir = points[0].getDirection() + (totalDist - points[0].getPosition())
+						* (AngleHelper.getSmallestAngleBetween(points[0].getDirection(), points[1].getDirection())
+								/ (points[1].getPosition() - points[0].getPosition()));
+				xft = points[0].xft + Math.cos(Math.toRadians(dir)) * (ds / 12);
+				yft = points[0].yft + Math.sin(Math.toDegrees(dir)) * (ds / 12);
+				toAdd = new PointonPath(totalDist, dir, xft, yft, t, maxVel, 0);
+			} else if (t < totalTime) {
+				System.out.println("accelTime < t < totalTime");
+				System.out.println("curVel: " + curVel);
+				System.out.println("totalDist: " + totalDist);
+				System.out.println("");
+				curVel = maxVel - maxAccel * (t - (accelTime + cruiseTime));
+				totalDist = accelDist + cruiseDist + maxVel * (t - (accelTime + cruiseTime))
+						- (maxAccel / 2) * Math.pow(t - (accelTime + cruiseTime), 2);
+				ds = totalDist - mappedPath.get(lastIndex).getPosition();
+				points = path.findSurroundingPoints(totalDist);
+				dir = points[0].getDirection() + (totalDist - points[0].getPosition())
+						* (AngleHelper.getSmallestAngleBetween(points[0].getDirection(), points[1].getDirection())
+								/ (points[1].getPosition() - points[0].getPosition()));
+				xft = points[0].xft + Math.cos(Math.toRadians(dir)) * (ds / 12);
+				yft = points[0].yft + Math.sin(Math.toDegrees(dir)) * (ds / 12);
+				toAdd = new PointonPath(totalDist, dir, xft, yft, t, curVel, -maxAccel);
+			} else {
+				System.out.println("Time should not be greater than totalTime: " + t);
+				toAdd = null;
+			}
+			mappedPath.add(toAdd);
+			lastIndex++;
 		}
-		profile.add(makeNode(totalTime));
+		mappedPath.add(new PointonPath(distance, path.get(path.numPoints() - 1).getDirection(),
+				path.get(path.numPoints() - 1).xft, path.get(path.numPoints() - 1).yft, totalTime, 0, 0)); // end point
+		System.out.println("Total accumulated distance: " + totalDist);
 	}
-	
-	private Node makeNode(double time) {
-		if (time <= accelTime) {
-			curVel = maxAccel * time;
-			return new Node(time, (maxAccel / 2) * Math.pow(time, 2), curVel, maxAccel);
-		} else if (time <= accelTime + cruiseTime) {
-			return new Node(time, accelDist + maxVel * (time - accelTime), maxVel, 0);
-		} else if (time < totalTime) {
-			return new Node(time, accelDist + cruiseDist + curVel * (time - (accelTime + cruiseTime)) + (-maxAccel / 2) * Math.pow(time - (accelTime + cruiseTime), 2),curVel - (maxAccel * (time - (accelTime + cruiseTime))), -maxAccel);
-		} else  {
-			return new Node(time, distance, 0, 0); // end node
-		}
-	}
-	
+
+	/*
+	 * private PointonPath makePoint(double time) { PointonPath[] points; double ds;
+	 * double dir; double xft; double yft; if (time <= accelTime) { curVel =
+	 * maxAccel * time; ds = (maxAccel / 2) * Math.pow(time, 2); totalDist += ds;
+	 * points = path.findSurroundingPoints(totalDist); dir =
+	 * points[0].getDirection() + (ds - points[0].getPosition()) *
+	 * (AngleHelper.getSmallestAngleBetween(points[0].getDirection(),
+	 * points[1].getDirection()) / (points[1].getPosition() -
+	 * points[0].getPosition())); xft = points[0].xft +
+	 * Math.cos(Math.toRadians(dir)) * ds; yft = points[0].xft +
+	 * Math.sin(Math.toDegrees(dir)) * ds; return new PointonPath(totalDist, dir,
+	 * xft, yft, time, curVel, maxAccel); } else if (time <= accelTime + cruiseTime)
+	 * { ds = maxVel * (time - accelTime); totalDist += ds; points =
+	 * path.findSurroundingPoints(totalDist); dir = points[0].getDirection() + (ds -
+	 * points[0].getPosition()) *
+	 * (AngleHelper.getSmallestAngleBetween(points[0].getDirection(),
+	 * points[1].getDirection()) / (points[1].getPosition() -
+	 * points[0].getPosition())); xft = points[0].xft +
+	 * Math.cos(Math.toRadians(dir)) * ds; yft = points[0].xft +
+	 * Math.sin(Math.toDegrees(dir)) * ds; return new PointonPath(totalDist, dir,
+	 * xft, yft, time, maxVel, 0); } else if (time < totalTime) { ds = curVel *
+	 * (time - (accelTime + cruiseTime)) + (-maxAccel / 2) * Math.pow(time -
+	 * (accelTime + cruiseTime), 2); totalDist += ds; points =
+	 * path.findSurroundingPoints(totalDist); dir = points[0].getDirection() + (ds -
+	 * points[0].getPosition()) *
+	 * (AngleHelper.getSmallestAngleBetween(points[0].getDirection(),
+	 * points[1].getDirection()) / (points[1].getPosition() -
+	 * points[0].getPosition())); xft = points[0].xft +
+	 * Math.cos(Math.toRadians(dir)) * ds; yft = points[0].xft +
+	 * Math.sin(Math.toDegrees(dir)) * ds; return new PointonPath(totalDist, dir,
+	 * xft, yft, time, maxVel - maxAccel * (time - (accelTime + cruiseTime))) return
+	 * new Node(time, accelDist + cruiseDist + curVel * (time - (accelTime +
+	 * cruiseTime)) + (-maxAccel / 2) * Math.pow(time - (accelTime + cruiseTime),
+	 * 2),curVel - (maxAccel * (time - (accelTime + cruiseTime))), -maxAccel); }
+	 * else { return new Node(time, distance, 0, 0); // end node } }
+	 */
+
 	public String toString() {
 		String toPrint = "";
-		for (Node point : profile) {
+		for (PointonPath point : mappedPath.getPoints()) {
 			toPrint += point + System.lineSeparator();
 		}
 		return toPrint;
 	}
-	
+
 	public double getDt() {
 		return dt;
 	}
-	
+
 	public double getMaxVel() {
 		return maxVel;
 	}
-	
-	public ArrayList<Node> getNodes() {
-		return profile;
+
+	public ArrayList<PointonPath> getMappedPoints() {
+		return mappedPath.getPoints();
 	}
-	
+
 }
--- 2018-Robot/src/org/usfirst/frc/team2849/util/AngleHelper.java
+++ 2018-Robot/src/org/usfirst/frc/team2849/util/AngleHelper.java
@@ -3,9 +3,6 @@ package org.usfirst.frc.team2849.util;
 public class AngleHelper {
 	
 	public static double getSmallestAngleBetween(double angle1, double angle2) {
-		if (angle1 > angle2) {
-			angle2 += 360;
-		}
 		double diff = Math.abs(angle2 - angle1);
 		if (diff > 180) {
 			return Math.signum(angle2 - angle1) * (360 - diff);
