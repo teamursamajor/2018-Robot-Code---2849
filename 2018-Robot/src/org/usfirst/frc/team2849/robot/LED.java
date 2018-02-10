f9c9070a5c1fcea1bab7b5db6fbc754a8254f6da
hgopal0252 <hgopal0252@HAHW8K9JXV1.inst.hcpss.org>
2018-02-10 14:09:16 -0500

    LED set color update

--- 2018-Robot/src/org/usfirst/frc/team2849/robot/LED.java
+++ 2018-Robot/src/org/usfirst/frc/team2849/robot/LED.java
@@ -11,6 +11,7 @@ public class LED extends Thread {
 	private Solenoid  gLED;
 	private Solenoid  bLED;
 	
+	
 	public LED(ControlLayout cont) {
 		this.cont = cont;
 		rLED = new Solenoid(0);
@@ -29,7 +30,34 @@ public class LED extends Thread {
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
+			setRGB(31,18,31);
 		}
 	}
+	public void setRGB(int rPercent, int gPercent, int bPercent) {
+		for(int n = 1; n<100;n++) {
+			if(n == rPercent) {
+				rLED.set(true);
+			}
+			if(n != rPercent) {
+				rLED.set(false);
+			}
+			if(n == gPercent) {
+				gLED.set(true);
+			}
+			if(n != gPercent) {
+				gLED.set(false);
+			}
+			if(n == bPercent) {
+				bLED.set(true);
+			}
+			if(n != bPercent) {
+				bLED.set(false);
+			}
+		}
+		
+	}
+	
+	
+	
 	
 }
